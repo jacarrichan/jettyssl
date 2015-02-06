3ac179a4dde71d409b9d91596c97f0c00d8d70d2
Jacarri Chan <772929989@qq.com>
2015-02-06 14:28:28 +0800

    add  reponse text
    Signed-off-by: Jacarri Chan <772929989@qq.com>

--- src/main/java/com/jacarrichan/research/jettyssl/web/SimpleServlet.java
+++ src/main/java/com/jacarrichan/research/jettyssl/web/SimpleServlet.java
@@ -1,6 +1,8 @@
 package com.jacarrichan.research.jettyssl.web;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
@@ -17,6 +19,11 @@ public class SimpleServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException,
             IOException {
+        resp.setContentType("text/html;charset=UTF-8");// 这条语句指明了向客户端发送的内容格式和采用的字符编码．
+        PrintWriter out = resp.getWriter();
+        out.println(" HELLO WORLD" + (new Date()));// 利用PrintWriter对象的方法将数据发送给客户端
+        out.flush();
+        out.close();
         System.out.println("doGet MyServlet");
     }
 
